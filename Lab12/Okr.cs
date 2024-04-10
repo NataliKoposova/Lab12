857e83a2f4bc2ef8918f529ae5236f9f9c99013a
Koposova1 <Koposova1@KOPOSOVANA>
2024-04-10 10:06:48 +0300

    Добавьте файлы проекта.

---
+++ Lab12/Okr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    internal static class Okr
+    {
+        public static double GetLength(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+        public static double GetSquare(double r)
+        {
+            return Math.PI * Math.Pow(r,2);
+        }
+        public static bool Point(double x, double y, double r)
+        {
+            if (Math.Abs(x) <= r && Math.Abs(y) <= r)
+                return true;
+            else
+                return false;
+        }
+    }   
+}
---
+++ Lab12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение радиуса");
+            double r = Convert.ToDouble(Console.ReadLine());            
+            Console.WriteLine("Введите координаты точки");
+            double px = Convert.ToDouble(Console.ReadLine());
+            double py = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Длина окружности равна {Okr.GetLength(r)}\nПлощаль окружности равна {Okr.GetSquare(r)}");
+            if (Okr.Point(px, py, r))
+                Console.WriteLine("Точка лежит внутри окружности");
+            else
+                Console.WriteLine("Точка не лежит внутри окружности");
+            Console.ReadKey();
+        }
+    }
+    
+}
